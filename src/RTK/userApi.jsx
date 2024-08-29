de0110245946927f026e89fd7985a1ecc622a41f
arunacharya1603 <arunacharya1603@gmail.com>
2024-08-29 22:25:54 +0530

    userApi Updated

--- src/RTK/userApi.jsx
+++ src/RTK/userApi.jsx
@@ -1,16 +1,22 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"; // Correct path
+// userApi.js
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://66ce9e69901aab24841efa2d.mockapi.io",
   }),
-  tagTypes: ['User'],
+  tagTypes: ["User"],
   endpoints: (builder) => ({
     getUsers: builder.query({
       query: () => "/crud",
       providesTags: (result) =>
-        result ? [...result.map(({ id }) => ({ type: 'User', id })), { type: 'User', id: 'LIST' }] : [{ type: 'User', id: 'LIST' }],
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "User", id })),
+              { type: "User", id: "LIST" },
+            ]
+          : [{ type: "User", id: "LIST" }],
     }),
     addUsers: builder.mutation({
       query: (user) => ({
@@ -18,23 +24,25 @@ export const userApi = createApi({
         method: "POST",
         body: user,
       }),
-      invalidatesTags: [{ type: 'User', id: 'LIST' }],
+      invalidatesTags: [{ type: "User", id: "LIST" }],
     }),
     deleteUser: builder.mutation({
       query: (id) => ({
         url: `/crud/${id}`,
         method: "DELETE",
       }),
-      invalidatesTags: (result, error, id) => [{ type: 'User', id }], // Invalidate the specific user
+      invalidatesTags: (result, error, id) => [{ type: "User", id }],
     }),
-    searchUser: builder.mutation({
-      query: (name) => ({
-        url: `/crud?name=${name}`,
-        method: "GET",
-      }),
-      providesTags: (result, error, name) => [{ type: 'User', id: 'LIST' }], // Invalidate the entire list
-    })
+    searchUsers: builder.query({
+      query: (name) => `/crud?name=${name}`,
+      providesTags: (result, error, name) => [{ type: "User", id: "LIST" }],
+    }),
   }),
 });
 
-export const { useGetUsersQuery, useAddUsersMutation, useDeleteUserMutation, useSearchUserMutation } = userApi;
+export const {
+  useGetUsersQuery,
+  useAddUsersMutation,
+  useDeleteUserMutation,
+  useSearchUsersQuery,
+} = userApi;
